e2dfe35a17f95827cc5f3d1f10b568a1cf3a2c72
erechorse <erechorse@gmail.com>
2026-02-14 14:52:30 +0000

    feat: enable Tailscale SSH support on WSL

--- hosts/wsl/default.nix
+++ hosts/wsl/default.nix
@@ -12,6 +12,12 @@
     shell = pkgs.zsh;
   };
 
+  services.tailscale.enable = true;
+
+  services.openssh.enable = true;
+
+  networking.firewall.checkReversePath = "loose";
+
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   system.stateVersion = "25.05";
